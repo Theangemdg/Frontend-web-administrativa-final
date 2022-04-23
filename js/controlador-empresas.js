26ce7ca6e1399f394ba5a8344b3d75744b18b120
theangemdg <danielsanchez@unah.hn>
2022-04-23 00:14:49 -0600

    implementada la api para las empresas a la web administrativa

--- js/controlador-empresas.js
+++ js/controlador-empresas.js
@@ -9,13 +9,79 @@ var categoriaActiva = JSON.parse(sessionStorage.getItem('Categoria Seleccionada'
 
 function llenarTablaEmpresas(){
     axios({
-        url: 'http://localhost/Backend-Portal-Delivery/api/empresas.php'+categoriaActiva.id-1,
+        url: 'http://localhost/Backend-Portal-Delivery/api/empresas.php?id='+(categoriaActiva.id-1),
         method: 'get',
         responseType: 'json'
     }).then((res) =>{
         console.log(res.data);
+        document.getElementById('bodyempresas').innerHTML = "";
+        for(let i=0; i<res.data.empresas.length; i++){
+            document.getElementById('bodyempresas').innerHTML += 
+            `
+            <tr>
+                <td>${i+1}</td>
+                <td><img src="${res.data.empresas[i].logo}" alt="" height="30px"></td>
+                <td>${res.data.empresas[i].nombreEmpresa}</td>
+                <td>
+                    <a class="btn btn-info" href="../html/editarEmpresa.html" onclick="empresaSeleccionada('${res.data.id}', '${i}')">
+                        <i class="fas fa-pen"></i>
+                    </a>
+                    <button class="btn btn-danger" onclick="eliminarEmpresa('${res.data.id}', '${i}')">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </td>
+            </tr>
+            `
+        }
     }).catch(err=>{
         console.log(err);
     })
 }
-llenarTablaCategories();
+llenarTablaEmpresas();
+
+function empresaSeleccionada(idCategoria,idEmpresa){
+
+    axios({
+        url: 'http://localhost/Backend-Portal-Delivery/api/empresas.php?id='+(idCategoria-1)+'&idE='+idEmpresa,
+        method: 'get',
+        responseType: 'json'
+    }).then((res) =>{
+        console.log(res)
+        sessionStorage.setItem('empresa seleccionada', JSON.stringify(res.data));
+    }).catch(err=>{
+        console.log(err);
+    })
+}
+
+function eliminarEmpresa(idCategoria, idEmpresa){
+    console.log(idCategoria);
+    console.log(idEmpresa);
+    Swal.fire({
+        title: 'Estas Seguro?',
+        text: "No podras revertir los cambios",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#44bae6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, estoy seguro!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            axios({
+                url: 'http://localhost/Backend-Portal-Delivery/api/empresas.php?id='+(idCategoria-1)+'&idE='+idEmpresa,
+                method: 'delete',
+                responseType: 'json'
+            }).then((res) =>{
+               llenarTablaEmpresas();
+
+               Swal.fire({
+                icon: 'success',
+                title: 'Eliminada!',
+                text: 'La empresa ha sido eliminada',
+                confirmButtonColor: '#44bae6' 
+                })
+            }).catch(err=>{
+                console.log(err);
+            })
+        }
+    })
+}
---
+++ js/editar-empresas.js
@@ -0,0 +1,95 @@
+var clienteActivo = JSON.parse(sessionStorage.getItem('Usuario activo'));
+document.getElementById('userDropdown').innerHTML = "";
+document.getElementById('userDropdown').innerHTML = 
+`
+<span class="mr-2 d-none d-lg-inline text-gray-600 small">${clienteActivo.nombre}</span>
+<img class="img-profile rounded-circle" src="../img/undraw_profile.svg">
+`
+
+var categoriaActiva = JSON.parse(sessionStorage.getItem('Categoria Seleccionada'));
+var empresaActiva = JSON.parse(sessionStorage.getItem('empresa seleccionada'));
+document.getElementById('nombre').value = empresaActiva.nombreEmpresa;
+document.getElementById('logo').value = empresaActiva.logo;
+document.getElementById('banner').value = empresaActiva.imagen;
+document.getElementById('descripcion').value = empresaActiva.descripcion;
+
+axios({
+    url: 'http://localhost/Backend-Portal-Delivery/api/productos.php?id='+(categoriaActiva.id-1)+'&idE='+(empresaActiva.id-1),
+    method: 'get',
+    responseType: 'json'
+}).then((res) =>{
+    console.log(res)
+    document.getElementById('productos').innerHTML = "";
+    for(let i=0; i<res.data.productos.length; i++){
+        document.getElementById('productos').innerHTML +=
+        `<option value="${i}">${res.data.productos[i].nombreProducto}</option>
+        `
+    }
+}).catch(err=>{
+    console.log(err);
+})
+
+
+function editarEmpresa(){ 
+    let txtnombreEmpresa = document.getElementById('nombre').value;
+    let txtLogo = document.getElementById('logo').value;
+    let txtbanner = document.getElementById('banner').value;
+    let txtdescripcion = document.getElementById('descripcion').value;
+
+    if(txtnombreEmpresa && txtLogo && txtbanner && txtdescripcion){
+        axios({
+            url: 'http://localhost/Backend-Portal-Delivery/api/productos.php?id='+(categoriaActiva.id-1)+'&idE='+(empresaActiva.id-1),
+            method: 'get',
+            responseType: 'json'
+        }).then((res) =>{
+            console.log(res)
+            let productos = Array();
+            for(let i=0; i<res.data.productos.length; i++){
+                productos.push(
+                    {
+                        nombreProducto: res.data.productos[i].nombreProducto,
+                        imagenProducto: res.data.productos[i].imagenProducto,
+                        descripcion: res.data.productos[i].descripcion,
+                        precio: res.data.productos[i].precio
+                    }
+                )
+            }
+            let empresa = {
+                id: res.data.id,
+                nombreEmpresa:  document.getElementById('nombre').value,
+                imagen:  document.getElementById('banner').value,
+                logo: document.getElementById('logo').value,
+                descripcion: document.getElementById('descripcion').value,
+                productos: productos
+            }
+
+            axios({
+                url: 'http://localhost/Backend-Portal-Delivery/api/empresas.php?id='+(categoriaActiva.id-1)+'&idE='+(empresaActiva.id-1),
+                method: 'put',
+                responseType: 'json',
+                data: empresa
+            }).then((res) =>{
+                console.log(res.data);
+            }).catch(err=>{
+                console.log(err);
+            })
+            vaciarSessionStorage();
+            window.location  = "../html/adminEmpresas.html"
+            
+        }).catch(err=>{
+            console.log(err);
+        })
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'debes rellenar todos los campos',
+            confirmButtonColor: '#44bae6' 
+        })
+    }
+    
+}
+
+function vaciarSessionStorage(){
+    sessionStorage.setItem('empresa seleccionada', "");
+}
